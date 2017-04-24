d84fb61b92c0f19942e0614c2d1f91ac9937cacd
Jennifer Barry <barry.159@osu.edu>
2017-04-24 06:24:57 -0400

    Removing unnecessary ensemble learners, working on adding in comments, Playoffs predictions

--- pythonClassifiers.py
+++ pythonClassifiers.py
@@ -3,17 +3,13 @@ from sklearn.naive_bayes import GaussianNB #Naive Bayes
 from sklearn.neural_network import MLPClassifier #Artificial Neural Network
 from sklearn import svm #Support Vector Machine
 #Ensemble Classifiers:
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.ensemble import BaggingClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import VotingClassifier
-from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import AdaBoostClassifier #Adaboost
+from sklearn.ensemble import VotingClassifier #Voting
 #Preprocessing:
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import LabelBinarizer
 
 import sklearn.metrics as metrics
-from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import cross_val_predict
 import csv
 import numpy
@@ -21,6 +17,7 @@ import math
 import random
 from statistics import mean
 
+#Calculate F-Measure based on a provided confusion matrix
 def fMeasure(confusion_matrix):
 	tp = confusion_matrix[0][0]
 	fp = confusion_matrix[1][0]
@@ -29,11 +26,14 @@ def fMeasure(confusion_matrix):
 	recall = tp/(tp+fn)
 	return (2*((precision*recall)/(precision+recall)))
 
+#Given a set of records and true classes, determine the optimal classifier based on 10-fold cross-validation
 def classifiers(records, classes):
+	#Initialize variables to store best classifier's info
 	best_classifier = ""
 	accuracy = 0
 	f_measure = 0
 
+	#Decision Tree Classifier
 	decision_tree = DecisionTreeClassifier(criterion="entropy")
 	dt_prediction = cross_val_predict(decision_tree, records, classes, cv=10)
 	dt_accuracy = metrics.accuracy_score(classes, dt_prediction)
@@ -44,6 +44,7 @@ def classifiers(records, classes):
 	print("Confusion Matrix:", dt_confusion_matrix)
 	print("F-measure:", dt_fmeasure)'''
 
+	#Determine if classifier is optimal of those tested so far
 	if dt_accuracy > accuracy and dt_fmeasure > f_measure:
 		best_classifier = "Decision Tree"
 		accuracy = dt_accuracy
@@ -59,6 +60,7 @@ def classifiers(records, classes):
 	print("Confusion Matrix:", nb_confusion_matrix)
 	print("F-measure:", nb_fmeasure)'''
 
+	#Determine if classifier is optimal of those tested so far
 	if nb_accuracy > accuracy and nb_fmeasure > f_measure:
 		best_classifier = "Naive Bayes"
 		accuracy = nb_accuracy
@@ -74,6 +76,7 @@ def classifiers(records, classes):
 	print("Confusion Matrix:", ann_confusion_matrix)
 	print("F-measure:", ann_fmeasure)'''
 
+	#Determine if classifier is optimal of those tested so far
 	if ann_accuracy > accuracy and ann_fmeasure > f_measure:
 		best_classifier = "Artificial Neural Net"
 		accuracy = ann_accuracy
@@ -89,12 +92,13 @@ def classifiers(records, classes):
 	print("Confusion Matrix:", svm_confusion_matrix)
 	print("F-measure:", svm_fmeasure)'''
 
+	#Determine if classifier is optimal of those tested so far
 	if svm_accuracy > accuracy and svm_fmeasure > f_measure:
 		best_classifier = "Support Vector Machine"
 		accuracy = svm_accuracy
 		f_measure = svm_fmeasure
 
-	ensemble = AdaBoostClassifier()
+	ensemble = AdaBoostClassifier(base_estimator=support_vm,algorithm='SAMME')
 	ensemble_prediction = cross_val_predict(ensemble, records, classes, cv=10)
 	ensemble_accuracy = metrics.accuracy_score(classes, ensemble_prediction)
 	ensemble_confusion_matrix = metrics.confusion_matrix(classes, ensemble_prediction)
@@ -109,56 +113,27 @@ def classifiers(records, classes):
 		accuracy = ensemble_accuracy
 		f_measure = ensemble_fmeasure
 
-	bagging = BaggingClassifier()
-	bagging_prediction = cross_val_predict(bagging, records, classes, cv=10)
-	bagging_accuracy = metrics.accuracy_score(classes, bagging_prediction)
-	bagging_confusion_matrix = metrics.confusion_matrix(classes, bagging_prediction)
-	bagging_fmeasure = fMeasure(bagging_confusion_matrix)
+	voting = VotingClassifier(estimators=[('dt',decision_tree),('gnb',naive_bayes),('svm',support_vm),('ann',ann)])
+	voting_prediction = cross_val_predict(voting, records, classes, cv=10)
+	voting_accuracy = metrics.accuracy_score(classes, voting_prediction)
+	voting_confusion_matrix = metrics.confusion_matrix(classes, voting_prediction)
+	voting_fmeasure = fMeasure(voting_confusion_matrix)
 
-	'''print("Ensemble (Bagging) Accuracy:", bagging_accuracy)
-	print("Confusion Matrix:", bagging_confusion_matrix)
-	print("F-measure:", bagging_fmeasure)'''
+	'''print("Ensemble (Voting) Accuracy:", voting_accuracy)
+	print("Confusion Matrix:", voting_confusion_matrix)
+	print("F-measure:", voting_fmeasure)'''
 
-	if bagging_accuracy > accuracy and bagging_fmeasure > f_measure:
-		best_classifier = "Bagging Classifier"
-		accuracy = bagging_accuracy
-		f_measure = bagging_fmeasure
-
-	forest = RandomForestClassifier()
-	forest_prediction = cross_val_predict(forest, records, classes, cv=10)
-	forest_accuracy = metrics.accuracy_score(classes, forest_prediction)
-	forest_confusion_matrix = metrics.confusion_matrix(classes, forest_prediction)
-	forest_fmeasure = fMeasure(forest_confusion_matrix)
-
-	'''print("Ensemble (Random Forest) Accuracy:", forest_accuracy)
-	print("Confusion Matrix:", forest_confusion_matrix)
-	print("F-measure:", forest_fmeasure)'''
-
-	if forest_accuracy > accuracy and forest_fmeasure > f_measure:
-		best_classifier = "Forest Classifier"
-		accuracy = forest_accuracy
-		f_measure = forest_fmeasure
-
-	boosting = GradientBoostingClassifier()
-	boosting_prediction = cross_val_predict(boosting, records, classes, cv=10)
-	boosting_accuracy = metrics.accuracy_score(classes, boosting_prediction)
-	boosting_confusion_matrix = metrics.confusion_matrix(classes, boosting_prediction)
-	boosting_fmeasure = fMeasure(boosting_confusion_matrix)
-
-	'''print("Ensemble (Gradient Boosting) Accuracy:", boosting_accuracy)
-	print("Confusion Matrix:", boosting_confusion_matrix)
-	print("F-measure:", boosting_fmeasure)'''
-
-	if boosting_accuracy > accuracy and boosting_fmeasure > f_measure:
-		best_classifier = "Gradient Boosting Classifier"
-		accuracy = boosting_accuracy
-		f_measure = boosting_fmeasure
+	#Determine if classifier is optimal of those tested so far
+	if voting_accuracy > accuracy and voting_fmeasure > f_measure:
+		best_classifier = "Voting Classifier"
+		accuracy = voting_accuracy
+		f_measure = voting_fmeasure
 
 	print("Most Accurate Classifier:", best_classifier)
 	print("Accuracy:",accuracy)
 	print("F-Measure",f_measure)
 
-#Open CSV file containing data set
+#Four Factors training set
 with open('2016_results.csv', "rt") as FourFactors_data:
 	FourFactors = csv.reader(FourFactors_data)
 	FourFactors = list(FourFactors)
@@ -175,8 +150,9 @@ with open('2016_results.csv', "rt") as FourFactors_data:
 	print("Four Factors:")
 	classifiers(records, classes)
 
-#Open CSV file containing data set
+#Four Factors w/ Opposing Teams training set
 with open('2016_results_opp_team.csv', "rt") as FourFactorsOpp_data:
+	#Read data from CSV file
 	FourFactorsOpp = csv.reader(FourFactorsOpp_data)
 	FourFactorsOpp = list(FourFactorsOpp)
 
@@ -193,8 +169,9 @@ with open('2016_results_opp_team.csv', "rt") as FourFactorsOpp_data:
 	print("Four Factors with Opposing Team Stats:")
 	classifiers(records, classes)
 
-#Open CSV file containing data set
+#Advanced Stats training set
 with open('2016_advanced_statistics.csv', "rt") as Advanced_data:
+	#Read data from CSV file
 	Advanced = csv.reader(Advanced_data)
 	Advanced = list(Advanced)
 
@@ -216,17 +193,18 @@ with open('2016_advanced_statistics.csv', "rt") as Advanced_data:
 	print("Advanced Stats:")
 	classifiers(records, classes)
 
-#Open CSV file containing data set
-with open('2016_advanced_results_opp_team.csv', "rt") as Advanced_data:
-	Advanced = csv.reader(Advanced_data)
-	Advanced = list(Advanced)
+#Advanced Stats w/ Opposing Teams training set
+with open('2016_advanced_results_opp_team.csv', "rt") as AdvancedOpp_data:
+	#Read data from CSV file
+	AdvancedOpp = csv.reader(AdvancedOpp_data)
+	AdvancedOpp = list(AdvancedOpp)
 
 	lb = LabelBinarizer()
 	lb.fit(['  away','  home'])
 
 	records = []
 	classes = []
-	for record in Advanced[1:]:
+	for record in AdvancedOpp[1:]:
 		attributes = numpy.array(record[1:5])
 		attributes = numpy.append(attributes, lb.transform([record[5]]))
 		attributes = numpy.append(attributes,numpy.array(record[6:9]).astype(numpy.float))
@@ -239,3 +217,71 @@ with open('2016_advanced_results_opp_team.csv', "rt") as Advanced_data:
 
 	print("Advanced Stats with Opposing Team Stats:")
 	classifiers(records, classes)
+
+#Testing on Playoff Data
+'''The optimal algorithm was determined through analysis of the results obtained through the "classifiers" function'''
+with open('playoff_statistics_predictions.csv', "rt") as Playoffs_data:
+	#Open CSV file
+	Playoffs = csv.reader(Playoffs_data)
+	Playoffs = list(Playoffs)
+
+	#Generate test datasets
+	test_records = []
+	test_classes = []
+	for record in Playoffs[1:]:
+		attributes = numpy.append(numpy.array(record[1:5]).astype(numpy.float),numpy.array(record[6:10]).astype(numpy.float))
+		test_records.append(attributes)
+		test_classes.append(record[10])
+
+	#Preformat the test data by scaling to a range of [0,1] and converting to a list
+	test_min_max = MinMaxScaler(feature_range=(0, 1), copy=False)
+	test_records = test_min_max.fit_transform(test_records)
+	test_records = test_records.tolist()
+
+	#Training data set: Four Factors w/ Opposing Teams
+	with open('2016_results_opp_team.csv', "rt") as training_data:
+		#Read data from CSV file
+		training = csv.reader(training_data)
+		training = list(training)
+
+		#Generate training datasets
+		training_records = []
+		training_classes = []
+		for record in training[1:]:
+			attributes = numpy.append(numpy.array(record[1:5]).astype(numpy.float),numpy.array(record[6:10]).astype(numpy.float))
+			training_records.append(attributes)
+			training_classes.append(record[10])
+
+		#Preformat the training data by scaling to a range of [0,1] and converting to a list
+		training_min_max = MinMaxScaler(feature_range=(0, 1), copy=False)
+		training_records = training_min_max.fit_transform(training_records)
+		training_records = training_records.tolist()
+
+		#Initialize Support Vector Machine (optimal algorithm based on analysis)
+		support_vm = svm.SVC(kernel='linear')
+
+		#Fit SVM to training data set
+		support_vm.fit(training_records, training_classes)
+
+		#Use model to predict the outcome of each game in the test dataset
+		predictions = support_vm.predict(test_records)
+		predictions = [classifier.strip(' ') for classifier in predictions]
+		
+		#Calculate performance measures
+		accuracy = metrics.accuracy_score(test_classes, predictions)
+		confusion_matrix = metrics.confusion_matrix(test_classes, predictions)
+		fmeasure = fMeasure(confusion_matrix)
+
+		#Print results
+		print("Playoffs Accuracy:", accuracy)
+		print("Confusion Matrix:", confusion_matrix)
+		print("F-measure:", fmeasure)
+
+		#Generate CSV file for analysis of outcomes
+		with open('playoffs_results.csv', 'w') as output_file:
+			#csv writers
+			writer = csv.writer(output_file)
+			writer.writerow(['Team Name','Opposing Team','Result','Prediction'])
+			for i in range(0,len(test_records)):
+				row = [Playoffs[i+1][0],Playoffs[i+1][5],test_classes[i],predictions[i]]
+				writer.writerow(row)
