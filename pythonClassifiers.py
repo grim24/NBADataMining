f978e7a67e836ae641cdad66a836558a120bb4b1
Jennifer Barry <barry.159@osu.edu>
2017-04-24 10:07:06 -0400

    You get a comment, you get a comment, EVERY BODY GETS A COMMENT

--- pythonClassifiers.py
+++ pythonClassifiers.py
@@ -35,7 +35,9 @@ def classifiers(records, classes):
 
 	#Decision Tree Classifier
 	decision_tree = DecisionTreeClassifier(criterion="entropy")
+	#Predict the class of each training record through 10-fold cross-validation
 	dt_prediction = cross_val_predict(decision_tree, records, classes, cv=10)
+	#Calculate performance measures
 	dt_accuracy = metrics.accuracy_score(classes, dt_prediction)
 	dt_confusion_matrix = metrics.confusion_matrix(classes, dt_prediction)
 	dt_fmeasure = fMeasure(dt_confusion_matrix)
@@ -50,8 +52,11 @@ def classifiers(records, classes):
 		accuracy = dt_accuracy
 		f_measure = dt_fmeasure
 
+	#Naive Bayes Classifier
 	naive_bayes = GaussianNB()
+	#Predict the class of each training record through 10-fold cross-validation
 	nb_prediction = cross_val_predict(naive_bayes, records, classes, cv=10)
+	#Calculate performance measures
 	nb_accuracy = metrics.accuracy_score(classes, nb_prediction)
 	nb_confusion_matrix = metrics.confusion_matrix(classes, nb_prediction)
 	nb_fmeasure = fMeasure(nb_confusion_matrix)
@@ -66,8 +71,11 @@ def classifiers(records, classes):
 		accuracy = nb_accuracy
 		f_measure = nb_fmeasure
 
+	#Artificial Neural Net
 	ann = MLPClassifier()
+	#Predict the class of each training record through 10-fold cross-validation
 	ann_prediction = cross_val_predict(ann, records, classes, cv=10)
+	#Calculate performance measures
 	ann_accuracy = metrics.accuracy_score(classes, ann_prediction)
 	ann_confusion_matrix = metrics.confusion_matrix(classes, ann_prediction)
 	ann_fmeasure = fMeasure(ann_confusion_matrix)
@@ -82,8 +90,11 @@ def classifiers(records, classes):
 		accuracy = ann_accuracy
 		f_measure = ann_fmeasure
 
+	#Support Vector Machine
 	support_vm = svm.SVC(kernel='linear')
+	#Predict the class of each training record through 10-fold cross-validation
 	svm_prediction = cross_val_predict(support_vm, records, classes, cv=10)
+	#Calculate performance measures
 	svm_accuracy = metrics.accuracy_score(classes, svm_prediction)
 	svm_confusion_matrix = metrics.confusion_matrix(classes, svm_prediction)
 	svm_fmeasure = fMeasure(svm_confusion_matrix)
@@ -98,8 +109,11 @@ def classifiers(records, classes):
 		accuracy = svm_accuracy
 		f_measure = svm_fmeasure
 
+	#Adaboost Classifier
 	ensemble = AdaBoostClassifier(base_estimator=support_vm,algorithm='SAMME')
+	#Predict the class of each training record through 10-fold cross-validation
 	ensemble_prediction = cross_val_predict(ensemble, records, classes, cv=10)
+	#Calculate performance measures
 	ensemble_accuracy = metrics.accuracy_score(classes, ensemble_prediction)
 	ensemble_confusion_matrix = metrics.confusion_matrix(classes, ensemble_prediction)
 	ensemble_fmeasure = fMeasure(ensemble_confusion_matrix)
@@ -108,13 +122,17 @@ def classifiers(records, classes):
 	print("Confusion Matrix:", ensemble_confusion_matrix)
 	print("F-measure:", ensemble_fmeasure)'''
 
+	#Determine if classifier is optimal of those tested so far
 	if ensemble_accuracy > accuracy and ensemble_fmeasure > f_measure:
 		best_classifier = "Adaboost Classifier"
 		accuracy = ensemble_accuracy
 		f_measure = ensemble_fmeasure
 
+	#Majority Voting Ensemble Classifier
 	voting = VotingClassifier(estimators=[('dt',decision_tree),('gnb',naive_bayes),('svm',support_vm),('ann',ann)])
+	#Predict the class of each training record through 10-fold cross-validation
 	voting_prediction = cross_val_predict(voting, records, classes, cv=10)
+	#Calculate performance measures
 	voting_accuracy = metrics.accuracy_score(classes, voting_prediction)
 	voting_confusion_matrix = metrics.confusion_matrix(classes, voting_prediction)
 	voting_fmeasure = fMeasure(voting_confusion_matrix)
@@ -135,15 +153,20 @@ def classifiers(records, classes):
 
 #Four Factors training set
 with open('2016_results.csv', "rt") as FourFactors_data:
+	#Read data from CSV file
 	FourFactors = csv.reader(FourFactors_data)
 	FourFactors = list(FourFactors)
 
+	#Generate training records and classes
 	records = []
 	classes = []
 	for record in FourFactors[1:]:
+		#Isolate desired attributes and add list of attributes to list of records
 		records.append(numpy.array(record[1:6]).astype(numpy.float))
+		#Add class to list of classes
 		classes.append(record[6])
 
+	#Preformat the training data by scaling to a range of [0,1]
 	min_max = MinMaxScaler(feature_range=(0, 1), copy=False)
 	records = min_max.fit_transform(records)
 
@@ -156,13 +179,18 @@ with open('2016_results_opp_team.csv', "rt") as FourFactorsOpp_data:
 	FourFactorsOpp = csv.reader(FourFactorsOpp_data)
 	FourFactorsOpp = list(FourFactorsOpp)
 
+	#Generate training records and classes
 	records = []
 	classes = []
 	for record in FourFactorsOpp[1:]:
+		#Isolate desired attributes
 		attributes = numpy.append(numpy.array(record[1:5]).astype(numpy.float),numpy.array(record[6:10]).astype(numpy.float))
+		#Add list of attributes to list of records
 		records.append(attributes)
+		#Add class to list of classes
 		classes.append(record[10])
 
+	#Preformat the training data by scaling to a range of [0,1]
 	min_max = MinMaxScaler(feature_range=(0, 1), copy=False)
 	records = min_max.fit_transform(records)
 
@@ -175,18 +203,26 @@ with open('2016_advanced_statistics.csv', "rt") as Advanced_data:
 	Advanced = csv.reader(Advanced_data)
 	Advanced = list(Advanced)
 
+	#Initialize preformatting for categorical attribute
 	lb = LabelBinarizer()
 	lb.fit([' away',' home'])
 
+	#Generate training records and classes
 	records = []
 	classes = []
+	#For each record in training data set,
 	for record in Advanced[1:]:
+		#Isolate desired attributes
 		attributes = numpy.array(record[2:6])
+		#Binarize categoirical attribute and add to list of record's attributes
 		attributes = numpy.append(attributes, lb.transform([record[6]]))
 		attributes = numpy.append(attributes,numpy.array(record[7:10]).astype(numpy.float))
+		#Add list of attributes to list of records
 		records.append(attributes)
+		#Add class to list of classes
 		classes.append(record[10])
 
+	#Preformat the training data by scaling to a range of [0,1]
 	min_max = MinMaxScaler(feature_range=(0, 1), copy=False)
 	records = min_max.fit_transform(records)
 
@@ -199,19 +235,27 @@ with open('2016_advanced_results_opp_team.csv', "rt") as AdvancedOpp_data:
 	AdvancedOpp = csv.reader(AdvancedOpp_data)
 	AdvancedOpp = list(AdvancedOpp)
 
+	#Initialize preformatting for categorical attribute
 	lb = LabelBinarizer()
 	lb.fit(['  away','  home'])
 
+	#Generate training records and classes
 	records = []
 	classes = []
+	#For each record in training data set,
 	for record in AdvancedOpp[1:]:
+		#Isolate desired attributes
 		attributes = numpy.array(record[1:5])
+		#Binarize categoirical attribute and add to list of record's attributes
 		attributes = numpy.append(attributes, lb.transform([record[5]]))
 		attributes = numpy.append(attributes,numpy.array(record[6:9]).astype(numpy.float))
 		attributes = numpy.append(attributes,numpy.array(record[10:17]).astype(numpy.float))
+		#Add list of attributes to list of records
 		records.append(attributes)
+		#Add class to list of classes
 		classes.append(record[17])
 
+	#Preformat the training data by scaling to a range of [0,1]
 	min_max = MinMaxScaler(feature_range=(0, 1), copy=False)
 	records = min_max.fit_transform(records)
 
